940bdd709fa7bffedb6dd1e7a6903ab56f10782c
krypc-sharath <sharathmohan@krypc.com>
2021-07-06 11:52:02 +0530

    New App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
-import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
-import { MathService } from './shared/math.service';
-import { SettingsService } from './shared/settings.service';
-import { SettingsComponent } from './settings/settings.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
-import { settingsReducer } from './store/settings.reducer';
-import { GameComponent } from './game/game.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SettingsComponent,
-    HomeComponent,
-    GameComponent,
-    AboutComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    HttpModule,
-    MaterialModule,
-    AppRoutingModule,
-    StoreModule.provideStore({ settings: settingsReducer })
+    BrowserModule
   ],
-  providers: [
-    MathService,
-    SettingsService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/store/app.state.ts
+++
@@ -1,5 +0,0 @@
-import { GameSettings } from './settings.reducer';
-
-export interface AppState {
-  settings: GameSettings;
-}
